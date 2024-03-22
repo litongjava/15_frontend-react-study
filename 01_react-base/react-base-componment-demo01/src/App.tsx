1b97e51d1d59692944f6400b5e0049783d7d6c77
litongjava <litongjava@qq.com>
2024-03-21 16:29:36 -1000

    add amis

---
+++ 01_react-base/react-base-componment-demo01/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {ReactNotifications} from 'react-notifications-component';
+import Homepage from './Homepage';
+
+function App() {
+  return (
+    <div>
+      <ReactNotifications/>
+      <Homepage/>
+    </div>
+  );
+};
+export default App;
